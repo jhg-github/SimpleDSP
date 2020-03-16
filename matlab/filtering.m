e8b01c972829caa3dfe682cacef00c7109f837eb
javi <javi@email.com>
2020-03-16 21:21:38 +0100

    update filtering

--- matlab/filtering.m
+++ matlab/filtering.m
@@ -1,17 +1,21 @@
-close all
-clear
+% close all
+% clear
 
 %-- common --
-Fs = 48000;           % Sampling frequency                    
-T = 1/Fs;             % Sampling period       
-L = 480;             % Length of signal
-t = (0:L-1)*T;        % Time vector
-f1 = 200;
-f2 = 400;
-f3 = 1000;
-f4 = 1200;
-f5 = 1800;
-f6 = 2000;
+fs = 48000;         % Sampling frequency                    
+Ts = 1/fs;          % Sampling period
+fnyq = fs / 2;      % Nyquist frequency
+L = 480;            % Length of signal
+t = (0:L-1)*Ts;     % Time vector
+f1 = 200;           % Signal frequency components
+f2 = 400;           %
+f3 = 1000;          %
+f4 = 1200;          %
+f5 = 1800;          %
+f6 = 2000;          %
+B = 2200;             % Signal bandwidth
+atten_db = 60;      % Attenuation [db]. Aprox. 10 bits [ENOB], atten = 10 * 6.02 + 1.76 
+decimation = 8;     % Decimation factor
 hlow_fpass = 600;
 hlow_fstop = 800;
 hband_fstop1 = 600;
@@ -24,28 +28,47 @@ nfir = 100;
 
 %-- signal --
 xn = sin(2*pi*f1*t) + sin(2*pi*f2*t) + sin(2*pi*f3*t) + sin(2*pi*f4*t) + sin(2*pi*f5*t) + sin(2*pi*f6*t);
+% xn = sin(2*pi*f1*t);
 
 %-- signal fft --
-f = Fs*(0:(L/2))/L;
+f = fs*(0:(L/2))/L;
 Xm = fft(xn);
 P2 = abs(Xm/L);
 P1 = P2(1:L/2+1);
 P1(2:end-1) = 2*P1(2:end-1);
 
-%-- lowpass filter --
-hl_f =   [0 (hlow_fpass/Fs) (hlow_fstop/Fs) 1];
-hl_mag = [1 1               0               0];
-hl_k = fir2(nfir,hl_f,hl_mag);
+%-- decimation filter --
+fs_dec = fs / decimation;   % Sampling frequency after decimation
+fstop = fs_dec - B;
+fpass = B;
+n_hdec = ceil( atten_db / ( 22 * ( fstop/fs - fpass/fs ) ) );
+n_hdec = 50;
+hdec_f =   [0 (fpass/fnyq) (fstop/fnyq) 1];
+hdec_mag = [1 0.7079          0.001      0.001];
+hdec_k = fir2(n_hdec,hdec_f,hdec_mag);
+hdec_k = fir1(n_hdec,(fpass/fnyq));
 
-%-- lowpass filter fft --
-Hlm = fft( [hl_k zeros(1, L - length(hl_k))] );
-Hlm_P2 = abs(Hlm/1);    % don't divide because filter coefficients are already scaled ???
-Hlm_P1 = Hlm_P2(1:L/2+1);
-Hlm_P1(2:end-1) = Hlm_P1(2:end-1);
+%-- decimation filter fft --
+Hdecm = fft( [hdec_k zeros(1, L - length(hdec_k))] );
+Hdecm_P2 = abs(Hdecm/1);    % don't divide because filter coefficients are already scaled ???
+Hdecm_P1 = Hdecm_P2(1:L/2+1);
+Hdecm_P1(2:end-1) = Hdecm_P1(2:end-1);
+Hdecm_db = 20*log10(Hdecm_P1);
+
+% %-- lowpass filter --
+% hl_f =   [0 (hlow_fpass/Fs) (hlow_fstop/Fs) 1];
+% hl_mag = [1 1               0               0];
+% hl_k = fir2(nfir,hl_f,hl_mag);
+% 
+% %-- lowpass filter fft --
+% Hlm = fft( [hl_k zeros(1, L - length(hl_k))] );
+% Hlm_P2 = abs(Hlm/1);    % don't divide because filter coefficients are already scaled ???
+% Hlm_P1 = Hlm_P2(1:L/2+1);
+% Hlm_P1(2:end-1) = Hlm_P1(2:end-1);
 
 
 %-- plots --
-figure
+%figure
 subplot(5,2,1)
 plot(t, xn)
 grid on
@@ -61,15 +84,29 @@ xlabel('f (Hz)')
 ylabel('|P1(f)|')
 
 subplot(5,2,3)
-stem((0:nfir), hl_k)
+stem((0:n_hdec), hdec_k)
 grid on
-title('Lowpass filter coefficients')
+title('Decimation filter coefficients')
 xlabel('k')
-ylabel('hl(k)')
+ylabel('hdec(k)')
 
 subplot(5,2,4)
-plot(f,Hlm_P1) 
+plot(f,Hdecm_db) 
 grid on
-title('Single-Sided Amplitude Spectrum of Hl(m)')
+title('Single-Sided Amplitude Spectrum of Hdec(m)')
 xlabel('f (Hz)')
-ylabel('|Hlm_P1(f)|')
+ylabel('|Hdecm_P1(db)|')
+
+% subplot(5,2,3)
+% stem((0:nfir), hl_k)
+% grid on
+% title('Lowpass filter coefficients')
+% xlabel('k')
+% ylabel('hl(k)')
+% 
+% subplot(5,2,4)
+% plot(f,Hlm_P1) 
+% grid on
+% title('Single-Sided Amplitude Spectrum of Hl(m)')
+% xlabel('f (Hz)')
+% ylabel('|Hlm_P1(f)|')
